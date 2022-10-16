b68600b40e2e02b31a5b9bc0b397fa7b6ee51aa7
Ray <raysam.gas@gmail.com>
2022-10-16 21:37:10 +0700

    exports producer

---
+++ src/api/exports/handler.js
@@ -0,0 +1,32 @@
+const autoBind = require('auto-bind');
+
+class ExportsHandler {
+  constructor(producerService, playlistsService, validator) {
+    this._producerService = producerService;
+    this._playlistsService = playlistsService;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postExportPlaylistsHandler(request, h) {
+    this._validator.validateExportPlaylistsPayload(request.payload);
+
+    const message = {
+      userId: request.auth.credentials.id,
+      targetEmail: request.payload.targetEmail,
+    };
+
+    await this._producerService.sendMessage('export:playlists', JSON.stringify(message));
+
+    const response = h.response({
+      status: 'success',
+      message: 'Permintaan Anda sedang kami proses',
+    });
+
+    response.code(201);
+    return response;
+  }
+}
+
+module.exports = ExportsHandler;
---
+++ src/api/exports/routes.js
@@ -0,0 +1,10 @@
+const routes = (handler) => [{
+  method: 'POST',
+  path: '/export/playlists/{id}',
+  handler: handler.postExportPlaylistsHandler,
+  options: {
+    auth: 'openmusic_jwt',
+  },
+}];
+
+module.exports = routes;
